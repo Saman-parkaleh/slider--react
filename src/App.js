5b53be0a90c290e5b6ca315db29f928c54931250
saman <ssaman.parkaleh76@gmail.com>
2023-05-20 20:43:23 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import Data from "./components/Data";
+import "./App.css";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [persen, setPersen] = useState(Data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let lastindex = persen.length - 1;
+    if (index < 0) {
+      setIndex(lastindex);
+    }
+    if (index > lastindex) {
+      setIndex(0);
+    }
+  }, [index, persen]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 10000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className=" text-center box  flex justify-center content-center grid  h-[100vh]">
+      <div className="title">
+        <samp className="font-bold text-2xl">نظرانت مشتریان</samp>
+      </div>
+      <div className=" boxcontaner     ">
+        {persen.map((pepole, persenindex) => {
+          const { id, image, name, title, qoute } = pepole;
+           
+          let position = "nextslide";
+          if (persenindex === index) {
+            position = "activeSlide";
+          }
+          if (
+            persenindex === index - 1 ||
+            (index === 0 && persenindex === persen.length - 1)
+          ) {
+            position = "lastslide";
+          }
+          return (
+            <article key={id} className={`${position} contaner m-10`}>
+              <img
+                className="w-[150px] h-[150px] rounded-[50%]   bg-blue-400   m-10"
+                src={image}
+                alt=""
+              />
+              <h1 className="text-red-700 text-xl">{name}</h1>
+              <p className="text-blue-800 m-5 text-base">{title}</p>
+              <p className="m-5 text-sm">{qoute}</p>
+            </article>
+          );
+        })}
+      </div>
+      <div className="relative">
+        <button
+          className="absolute bottom-[290px] border-red-500  right-[0] text-3xl"
+          onClick={() => setIndex(index + 1)}
         >
-          Learn React
-        </a>
-      </header>
+          <FiChevronRight />
+        </button>
+        <button
+          className="absolute bottom-[290px] left-[0] text-3xl"
+          onClick={() => setIndex(index - 1)}
+        >
+          <FiChevronLeft />
+        </button>
+      </div>
     </div>
   );
 }
